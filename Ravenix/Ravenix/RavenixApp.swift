0af675f1488cd11c05c0a9fc32f6ea420da47cf2
Lily Hamilton <lilyhamilton@Mac.lan>
2025-12-02 22:06:32 -0800

    Create initial Ravenix SwiftUI iOS project

---
+++ Ravenix/Ravenix/RavenixApp.swift
@@ -0,0 +1,17 @@
+//
+//  RavenixApp.swift
+//  Ravenix
+//
+//  Created by Lily Hamilton on 12/2/25.
+//
+
+import SwiftUI
+
+@main
+struct RavenixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
